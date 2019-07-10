9ff3320a3d007b2d4e78bb04951df6c6751e34f6
Michiel <michiel@kitania.be>
2019-07-10 16:47:03 +0200

    Added: command line argument to pass fix file; fixed: hashbang

--- scripts/arthub-index.sh
+++ scripts/arthub-index.sh
@@ -1,11 +1,17 @@
-#/bin/bash
+#!/bin/bash
 
-usage() { echo "Usage: $0 [-e <string>]" 1>&2; exit 1; }
+usage() {
+	echo "Usage: $0 -e <string> -l <string>" 1>&2;
+	exit 1;
+}
 
-while getopts ":e:" o; do
+while getopts ":e:l:" o; do
     case "${o}" in
         e)
-            e=${OPTARG}
+            e="${OPTARG}"
+            ;;
+        l)
+            l="${OPTARG}"
             ;;
         *)
             usage
@@ -14,14 +20,27 @@ while getopts ":e:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${e}" ]; then
+if [ -z "${e}" -o -z "${l}" ]; then
     usage
+fi
+
+if [ ! -f "${l}" -o ! -r "${l}" ]; then
+	if [[ "$l" =~ ^[a-z]{2,3}$ ]]; then
+		l="../fixes/datahub-oai-to-blacklight-solr-${l}.fix"
+		if [ ! -f "${l}" -o ! -r "${l}" ]; then
+			echo "The fix file ${l} does not exist or is not readable."
+			exit 1
+		fi
+	else
+		echo "The fix file ${l} does not exist or is not readable."
+		exit 1
+    fi
 fi
 
 # Fetch data from the OAI endpoint
 
 echo "Fetching data"
-catmandu convert OAI --url $e --handler lido to JSON --fix ../fixes/datahub-oai-to-blacklight-solr.fix > /tmp/bulk.json
+catmandu convert OAI --url "$e" --handler lido to JSON --fix "$l" > /tmp/bulk.json
 
 # Fetch XML raw data from the OAI endpoint and add it to the JSON blob.
 
