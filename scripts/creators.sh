e7f3cf348b989a1dcad3bf6b2e45fc2be0247217
Matthias Vandermaesen <matthias@colada.be>
2019-07-18 14:12:52 +0200

    Fixed: shell options.

--- scripts/creators.sh
+++ scripts/creators.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-usage() {
-	echo "Usage: $0 -f <string>" 1>&2;
-	exit 1;
-}
+#
+# 1. Check out https://github.com/VlaamseKunstcollectie/Authority-Files
+# 2. Check out https://github.com/VlaamseKunstcollectie/Imagehub-Fixes
+# 3. chmod +x creators.sh
+# 4. ./creators.sh -e ../../Authority-Files/CREATORS_UTF8.csv
+#
 
-while getopts ":e:l:" o; do
+usage() { echo "Usage: $0 [-e <string>]" 1>&2; exit 1; }
+
+while getopts ":e:" o; do
     case "${o}" in
-        f)
-            f="${OPTARG}"
+        e)
+            e=${OPTARG}
             ;;
         *)
             usage
@@ -17,5 +21,9 @@ while getopts ":e:l:" o; do
 done
 shift $((OPTIND-1))
 
+if [ -z "${e}" ]; then
+    usage
+fi
+
 echo "Generating Creators vocabulary.";
-catmandu import CSV to DBI --data_source dbi:SQLite:/tmp/import.CREATORS_UTF8.sqlite < "$f"
+catmandu import CSV to DBI --data_source dbi:SQLite:/tmp/import.CREATORS_UTF8.sqlite < $e
